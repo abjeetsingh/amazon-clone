e16adb59c4fd8b40ff5f51c6280ef8f5e1c62961
Abjeet Singh <abjeets50@gmail.com>
2023-08-17 02:00:26 +0530

    feat: card details

---
+++ public/data/cardData.js
@@ -0,0 +1,42 @@
+export default [
+  {
+   title : "We have a surprice for you",
+   img: "../images/home_grid_1.jpg",
+   link: "See terms and conditions"
+  },
+  {
+    title : "Watch the Rings of Power",
+    img: "../images/home_grid_2.jpg",
+    link: "Start Streaming now"
+   },
+   {
+    title : "Unlimited Streaming",
+    img: "../images/home_grid_3.jpg",
+    link: "Find out more"
+   },
+   {
+    title : "More titles to explore",
+    img: "../images/home_grid_4.jpg",
+    link: "Browse Kindle Unlimited"
+   },
+   {
+    title : "Shop Pet Supplies",
+    img: "../images/home_grid_5.jpg",
+    link: "See more"
+   },
+   {
+    title : "Spring Sale",
+    img: "../images/home_grid_6.jpg",
+    link: "See the deal"
+   },
+   {
+    title : "Echo Buds",
+    img: "../images/home_grid_7.jpg",
+    link: "See more"
+   },
+   {
+    title : "Family plan 3 months free",
+    img: "../images/home_grid_8.jpg",
+    link: "Learn more"
+   }   
+]
