834d2d5dc2f8afac7ca31052fa2cd01c61eb531c
Abjeet Singh <abjeets50@gmail.com>
2023-08-17 02:01:02 +0530

    refactor: add modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**.{js,js,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        amazonclone:{
+          background: "#EAEDED",
+          light_blue: '#232F3A',
+          yellow: '#FEBD69',
+          DEFAULT: '#131921'
+        }
+      }
+    },
   },
   plugins: [],
 }
-
