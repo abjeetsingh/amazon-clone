ea083182679fe8de4882e1bf093130105b1b98d7
Abjeet Singh <abjeets50@gmail.com>
2023-08-20 23:38:57 +0530

    feat: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import "./App.css"
-import { HomePage } from './components'
+import { HomePage,Navbar, Checkout, SearchResults, ProductPage } from './components'
 export const App = () => {
   return (
     <Router>
+      <Navbar/>
       <Routes>
         <Route exact path='/' element={<HomePage/>}/>
+        <Route exact path='/search' element={<SearchResults/>}/>
+        <Route exact path='/product/:id' element={<ProductPage/>}/>
+        <Route exact path='/checkout' element={<Checkout/>}/>
+        
       </Routes>
     </Router>
   )
