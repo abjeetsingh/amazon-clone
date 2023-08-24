a1f6606ceaf5bbae2f7c8b45591aff006c8f5111
Abjeet Singh <abjeets50@gmail.com>
2023-08-24 16:26:16 +0530

    feat: add links to items

--- src/components/CarouselCategory.jsx
+++ src/components/CarouselCategory.jsx
@@ -1,5 +1,5 @@
 import { Navigation, Pagination,Autoplay, Scrollbar, A11y } from 'swiper/modules';
-
+import { createSearchParams, useNavigate } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
@@ -9,6 +9,17 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
 const CarouselCategory = () => {
+  const navigate = useNavigate();
+  const searchCategory = (category) => (
+    navigate({
+      pathname: "search",
+      search: `${createSearchParams({
+        category: `${category}`,
+        searchterm: ``
+      })}`
+    })
+  )
+
   return (
     <div className='bg-white'>
       <div className='text-2xl font-semibold p-3'>Shop by Category</div>
@@ -20,22 +31,22 @@ const CarouselCategory = () => {
         // className='h-[50%]'
         
       >
-        <SwiperSlide>
+        <SwiperSlide onClick={() => searchCategory("Deals")} className=' cursor-pointer'>
           <img src="../images/category_0.jpg" alt="" />
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide onClick={() => searchCategory("Amazon")} className=' cursor-pointer'>
           <img src="../images/category_1.jpg" alt="" />
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide onClick={() => searchCategory("Fashion")} className=' cursor-pointer'>
           <img src="../images/category_2.jpg" alt="" />
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide onClick={() => searchCategory("Computers")} className=' cursor-pointer'>
           <img src="../images/category_3.jpg" alt="" />
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide onClick={() => searchCategory("Home")} className=' cursor-pointer'>
           <img src="../images/category_4.jpg" alt="" />
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide onClick={() => searchCategory("Hobies")} className=' cursor-pointer'>
           <img src="../images/category_5.jpg" alt="" />
         </SwiperSlide>
       </Swiper>
